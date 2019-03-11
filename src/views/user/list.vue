4c53fb4fcecd85914bafff15c7f9c58824221a4e
devilkun <zwy876910200@qq.com>
2019-03-11 11:42:36 +0800

    修复因导航切换导致用户列表页面显示不完整的问题

--- src/views/user/list.vue
+++ src/views/user/list.vue
@@ -2,33 +2,33 @@
   <div class="app-container">
 
     <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%">
-      <el-table-column align="center" label="ID" width="130">
+      <el-table-column align="center" label="ID" width="100">
         <template slot-scope="scope">
           <span>{{ scope.row.id }}</span>
         </template>
       </el-table-column>
-      <el-table-column width="270px" align="center" label="用户名">
+      <el-table-column width="250px" align="center" label="用户名">
         <template slot-scope="scope">
           <span>{{ scope.row.author }}</span>
         </template>
       </el-table-column>
-      <el-table-column width="400px" align="center" label="添加日期">
+      <el-table-column align="center" label="添加日期" min-width="360px">
         <template slot-scope="scope">
           <span>{{ scope.row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column width="270px" label="重要程度" >
+      <el-table-column width="250px" label="重要程度" >
         <template slot-scope="scope">
           <svg-icon v-for="n in +scope.row.importance" :key="n" icon-class="star" class="meta-item__icon"/>
         </template>
       </el-table-column>
 
-      <el-table-column class-name="status-col" label="状态" width="230">
+      <el-table-column class-name="status-col" label="状态" width="200">
         <template slot-scope="scope">
           <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="操作" width="240">
+      <el-table-column align="center" label="操作" width="220">
         <template slot-scope="scope">
           <router-link :to="'/example/edit/'+scope.row.id">
             <el-button type="primary" size="small" icon="el-icon-edit">编辑</el-button>
