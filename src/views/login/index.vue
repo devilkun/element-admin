d0c28202ff6bcc82cb417950198076689c41a38d
devilkun <zwy876910200@qq.com>
2019-03-21 17:22:51 +0800

    更换登录页背景图片

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -169,7 +169,6 @@ export default {
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-$bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
 
@@ -177,7 +176,10 @@ $light_gray:#eee;
   position: fixed;
   height: 100%;
   width: 100%;
-  background-color: $bg;
+  margin:0px;
+  background: url(/src/assets/images/313698.jpg) no-repeat;
+  background-size:100% 100%;
+  background-attachment:fixed;
   .login-form {
     position: absolute;
     left: 0;
